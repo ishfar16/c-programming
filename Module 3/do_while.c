4ca6367b73a5df38100efaefdcf8d4a74d177407
ishfar16 <129704665+ishfar16@users.noreply.github.com>
2023-08-03 07:40:16 +0600

    Add files via upload

---
+++ Module 3/do_while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    i = 30;
+   do
+   {
+    printf("%d\n",i);
+    i=i+1;
+
+   }
+    while (i<=10);
+   
+    return 0;
+}
---
+++ Module 3/loop_2.c
@@ -0,0 +1,19 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int i;
+//     for(i=2;i<=20;i=i+1)
+//     printf("%d\n",i);
+//     return 0;
+
+// }
+
+#include<stdio.h>
+int main()
+{
+    for(int i=2;i<=20;i+=2)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
